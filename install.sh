3be4112efd4d81739b2f268f1b54934d5ab5b94e
evilenx <evilenx@proton.me>
2025-08-20 11:01:58 -0500

    fix toml format

--- install.sh
+++ install.sh
@@ -6,12 +6,18 @@ git clone --depth 1 https://github.com/evilenx/clock.git "$TEMP_DIR"
 cd "$TEMP_DIR"
 cargo build --release
 mkdir -p ~/.config/clock
-[ -f ~/.config/clock/config.yml ] || echo "font_size: 80" > ~/.config/clock/config.yml
+
+if [ ! -f ~/.config/clock/config.toml ]; then
+    cat > ~/.config/clock/config.toml << EOF
+[settings]
+font_size = 80
+padding = 20.0
+auto_resize = true
+EOF
+fi
+
 mkdir -p ~/.cargo/bin
 cp target/release/clock ~/.cargo/bin/clock
 rm -rf "$TEMP_DIR"
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bash_profile
 source ~/.bash_profile
-
-
-
